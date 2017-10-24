110b996491071e7b7cb0dac70a93909af448eee4
alxvnt <alixvoinot1@gmail.com>
2017-10-24 16:10:21 +0200

    Premières méthodes

---
+++ class.py
@@ -0,0 +1,31 @@
+
+
+#class that generate the level
+class Map:
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+#class that generate the character
+class Character:
+
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def display_position(self):
+        pass
+    
+
+#class that generate objects
+class Object:
+
+    def __init__(self):
+        pass
+
+ 
